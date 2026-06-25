5ee48cfc2eb804e3407ae3424fe10867ca9ca801
trustybooker <trustybooker@gmail.com>
2026-06-25 19:49:19 -0400

    Add safe integration config scaffold

---
+++ assets/site-config.js
@@ -0,0 +1,16 @@
+window.FIFYNOW_SITE_CONFIG = {
+  bookingUrl: '',
+  analytics: {
+    provider: 'none',
+    googleAnalyticsId: '',
+    plausibleDomain: ''
+  },
+  payments: {
+    aiStarterPass: '',
+    aiJobProductivityPass: '',
+    businessAiReadinessAudit: '',
+    teamTrainingDeposit: '',
+    implementationReviewDeposit: '',
+    aiReadinessLab: ''
+  }
+};
